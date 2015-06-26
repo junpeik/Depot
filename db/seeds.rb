eea15bd19cd3b62ae6edbc130936316c1be533b8
Junpei Kusumoto <kusumoto@Junpei-no-MacBook-Pro.local>
2015-06-26 10:41:31 +0900

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+Product.delete_all
+#...
+Product.create(title: 'Programming Ruby 1.9',
+	description:
+	  	  "Ruby is fastest growing and most exciting dynamic language out there. If you need to get working programs delivered fast, you should add Ruby to your toolbox.",
+	image_url: 'ruby.jpg',
+	price: 49.95)
+#...
